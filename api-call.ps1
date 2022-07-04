2008b0dae0561c16664bf4dc878663db97a7af8c
Donny Johnson <itchychips@itchyeyes.net>
2022-07-04 16:08:27 -0500

    Implement enough to get market gaps.

--- api-call.ps1
+++ api-call.ps1
@@ -13,20 +13,26 @@
 # Eve-PS. If not, see <https://www.gnu.org/licenses/>.
 [CmdletBinding()]
 Param(
+    [parameter()]
+    [switch]$ClearCache
 )
+
+Import-Module PSSQLite
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 $EsiBaseUri = "https://esi.evetech.net/latest"
 
-$global:LastHeaders = @{}
-# Cache using: $Cache[$Uri] = $Expiry
-$global:CacheExpiry = @{}
-# Cache using: $Cache[$Uri] = $Etag
-$global:CacheETag = @{}
-# Cache using: $Cache[$Uri] = $Response
-$global:CacheResponse = @{}
+if (-not $CacheExpiry -or $ClearCache) {
+    $global:LastHeaders = @{}
+    # Cache using: $Cache[$Uri] = $Expiry
+    $global:CacheExpiry = @{}
+    # Cache using: $Cache[$Uri] = $Etag
+    $global:CacheETag = @{}
+    # Cache using: $Cache[$Uri] = $Response
+    $global:CacheResponse = @{}
+}
 
 function Invoke-WebRequest2 {
     [CmdletBinding(DefaultParameterSetName="Body")]
@@ -175,34 +181,18 @@ function Get-EsiConstellation {
 function Get-EsiRegion {
     [CmdletBinding()]
     Param(
-        [parameter(Mandatory,ValueFromPipelineByPropertyName)]
-        [int]$RegionId
+        [parameter()]
+        [string]$Name="*",
+        [parameter()]
+        [int[]]$RegionId
     )
 
-    Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/regions/$RegionId/"
-}
-
-function Get-EsiMarketOrder {
-    [CmdletBinding()]
-    Param(
-        [parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="RegionId")]
-        [int]$RegionId,
-        [parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="SystemName")]
-        [string]$SystemName,
-        [parameter(ValueFromPipelineByPropertyName)]
-        [int]$ItemId
-    )
-
-    if ($SystemName) {
-        $RegionId = Get-EsiSystem $SystemName | Get-EsiConstellation | Get-EsiRegion | Select-Object -Expand RegionId
+    Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/regions/" | ForEach-Object {
+        Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/regions/$_/"
+    } | Where-Object {
+        $_.Name -like $Name -and
+        (-not $RegionId -or $RegionId -contains $_.RegionId)
     }
-
-    $queryParameter = ""
-    if ($ItemId) {
-        $queryParameter = "?type_id=$ItemId"
-    }
-
-    Invoke-WebRequest2 -Uri "$EsiBaseUri/markets/$RegionId/orders/$queryParameter"
 }
 
 function Search-EsiItem {
@@ -213,16 +203,6 @@ function Search-EsiItem {
     )
 
     Invoke-WebRequest2 -Uri "$EsiBaseUri/search/?categories=inventory_type&search=$Name" | Select-Object @{"Name"="ItemId"; "Expression"={$_.InventoryType}}
-}
-
-function Get-EsiGroup {
-    [CmdletBinding()]
-    Param(
-        [parameter()]
-        [string]$Name
-    )
-
-    Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/groups/"
 }
 
 function Convert-PSObjectProperty {
@@ -253,6 +233,290 @@ function Convert-PSObjectProperty {
         $output | Add-Member -Type NoteProperty -Name $propertyName -Value $property.Value
     }
     $output
+}
+
+function Get-EsiCategory {
+    [CmdletBinding()]
+    Param(
+        [parameter()]
+        [string]$Name="*"
+    )
+
+    $categoryIds = Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/categories"
+    $results = $categoryIds | ForEach-Object {
+        Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/categories/$_/"
+    } | Where-Object { $_.Name -like $Name }
+    $results
+}
+
+function Get-EsiGroup {
+    [CmdletBinding()]
+    Param(
+        [parameter()]
+        [string]$Name="*"
+    )
+
+    Process {
+        Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/groups/" | ForEach-Object {
+            Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/groups/$_/"
+        } | Where-Object { $_.Name -like $Name }
+    }
+}
+
+function Get-EsiType {
+    [CmdletBinding()]
+    Param(
+        [parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [int[]]$Types,
+        [parameter()]
+        [string]$Name="*"
+    )
+
+    Process {
+        if (-not $Types) {
+            $Types = Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/types/"
+        }
+        $Types | ForEach-Object {
+            Invoke-WebRequest2 -Uri "$EsiBaseUri/universe/types/$_/"
+        }
+    }
+}
+
+function Get-EsiMarketOrder {
+    [CmdletBinding()]
+    Param(
+        [parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [int]$RegionId,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$TypeId
+    )
+
+    Process {
+        $queryParameter = ""
+        if ($TypeId) {
+            $queryParameter = "?type_id=$TypeId"
+        }
+
+        Invoke-WebRequest2 -Uri "$EsiBaseUri/markets/$RegionId/orders/$queryParameter" | ForEach-Object {
+            $type = Get-EsiType -Types $_.type_id
+
+            # Not sure why this wasn't converted using Convert-PSObjectProperty.
+            $output = New-Object -Type PSObject
+            $output | Add-Member -Type NoteProperty -Name Duration -Value $_.duration
+            $output | Add-Member -Type NoteProperty -Name "IsBuyOrder" -Value $_.is_buy_order
+            $output | Add-Member -Type NoteProperty -Name "Issued" -Value $_.issued
+            $output | Add-Member -Type NoteProperty -Name "LocationId" -Value $_.location_id
+            $output | Add-Member -Type NoteProperty -Name "MinVolume" -Value $_.min_volume
+            $output | Add-Member -Type NoteProperty -Name "OrderId" -Value $_.order_id
+            $output | Add-Member -Type NoteProperty -Name "Price" -Value $_.price
+            if ($_.range -eq "station") {
+                $_.range = 0
+            }
+            elseif ($_.range -eq "solarsystem") {
+                $_.range = -1
+            }
+            elseif ($_.range -eq "region") {
+                $_.range = -2
+            }
+            $output | Add-Member -Type NoteProperty -Name "Range" -Value $_.range
+            $output | Add-Member -Type NoteProperty -Name "SystemId" -Value $_.system_id
+            $output | Add-Member -Type NoteProperty -Name "TypeId" -Value $_.type_id
+            $output | Add-Member -Type NoteProperty -Name "TypeName" -Value $type.Name
+            $output | Add-Member -Type NoteProperty -Name "TypeVolume" -Value $type.Volume
+            $output | Add-Member -Type NoteProperty -Name "VolumeRemain" -Value $_.volume_remain
+            $output | Add-Member -Type NoteProperty -Name "VolumeTotal" -Value $_.volume_total
+            $output
+        }
+    }
+}
+
+function New-EsiMarketOrderSearch {
+    [CmdletBinding()]
+    Param(
+        [parameter()]
+        [string]$RegionName="*",
+        [parameter()]
+        [string]$GroupName="*"
+    )
+
+    Process {
+        $regionIds = Get-EsiRegion -Name $RegionName | Select-Object -Expand RegionId
+        if (-not $regionIds) {
+            throw "No region IDs found.  Searched with: '$RegionName'."
+        }
+        $typeIds = Get-EsiGroup -Name $GroupName | Get-EsiType | Select-Object -Expand TypeId
+        if (-not $typeIds) {
+            throw "No type IDs found.  Searched with group: '$GroupName'."
+        }
+
+        foreach ($regionId in $regionIds) {
+            foreach ($typeId in $typeIds) {
+                $output = New-Object -Type PSObject
+                $output | Add-Member -Type NoteProperty -Name "RegionId" -Value $regionId
+                $output | Add-Member -Type NoteProperty -Name "TypeId" -Value $typeId
+                $output
+            }
+        }
+    }
+}
+
+function Create-EsiSavedMarketOrderTable {
+    [CmdletBinding()]
+    Param(
+        [parameter()]
+        [string]$DataSource="saved.sqlite"
+    )
+    $DataSource = Resolve-Path $DataSource
+    # I still can't believe I got all the types right here without
+    # documentation.
+    Invoke-SqliteQuery -DataSource $DataSource -Query "
+        CREATE TABLE IF NOT EXISTS market_order (
+            OrderId INT PRIMARY KEY,
+            Duration INT,
+            IsBuyOrder INT,
+            Issued DATETIME,
+            LocationId INT,
+            MinVolume INT,
+            Price DECIMAL,
+            Range INT,
+            SystemId INT,
+            TypeId INT,
+            TypeName STRING,
+            TypeVolume DECIMAL,
+            VolumeRemain INT,
+            VolumeTotal INT
+        );"
+}
+
+function Save-EsiMarketOrder {
+    [CmdletBinding()]
+    Param(
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$Duration,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [bool]$IsBuyOrder,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [datetime]$Issued,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [long]$LocationId,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$MinVolume,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [long]$OrderId,
+        [parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [decimal]$Price,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$Range,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$SystemId,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$TypeId,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [string]$TypeName,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [decimal]$TypeVolume,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$VolumeRemain,
+        [parameter(ValueFromPipelineByPropertyName)]
+        [int]$VolumeTotal,
+        [parameter()]
+        [string]$DataSource="saved.sqlite"
+    )
+    Begin {
+        $DataSource = Resolve-Path $DataSource
+        Create-EsiSavedMarketOrderTable -DataSource $DataSource
+    }
+
+    Process {
+        Invoke-SqliteQuery -DataSource $DataSource -Query "
+            INSERT OR REPLACE INTO market_order (
+                OrderId,
+                Duration,
+                IsBuyOrder,
+                Issued,
+                LocationId,
+                MinVolume,
+                Price,
+                Range,
+                SystemId,
+                TypeId,
+                TypeName,
+                TypeVolume,
+                VolumeRemain,
+                VolumeTotal
+            ) VALUES (
+                (SELECT OrderId FROM market_order WHERE OrderId = @OrderId),
+                @Duration,
+                @IsBuyOrder,
+                @Issued,
+                @LocationId,
+                @MinVolume,
+                @Price,
+                @Range,
+                @SystemId,
+                @TypeId,
+                @TypeName,
+                @TypeVolume,
+                @VolumeRemain,
+                @VolumeTotal
+            );" -SqlParameters @{
+                "OrderId"=$OrderId
+                "Duration"=$Duration
+                "IsBuyOrder"=$IsBuyOrder
+                "Issued"=$Issued
+                "LocationId"=$LocationId
+                "MinVolume"=$MinVolume
+                "Price"=$Price
+                "Range"=$Range
+                "SystemId"=$SystemId
+                "TypeId"=$TypeId
+                "TypeName"=$TypeName
+                "TypeVolume"=$TypeVolume
+                "VolumeRemain"=$VolumeRemain
+                "VolumeTotal"=$VolumeTotal
+            }
+    }
+}
+
+function Search-EsiMarketGaps {
+    [CmdletBinding()]
+    Param(
+        [parameter()]
+        [string]$DataSource="saved.sqlite"
+    )
+
+    try {
+        Set-StrictMode -Off
+        $DataSource = Resolve-Path $DataSource
+
+        $typeNames = Invoke-SqliteQuery -DataSource $DataSource -Query "
+            SELECT DISTINCT mo.TypeName
+            FROM market_order mo
+            ORDER BY mo.TypeName;" | Select-Object -Expand TypeName
+
+        foreach ($typeName in $typeNames) {
+            $buyOrders = Invoke-SqliteQuery -DataSource $DataSource -Query "
+                SELECT mo.TypeName, mo.Price, mo.VolumeRemain
+                FROM market_order mo
+                WHERE mo.IsBuyOrder = 1
+                    AND mo.TypeName = @TypeName
+                ORDER BY mo.Price DESC;" -SqlParameters @{"TypeName"=$typeName}
+            $sellOrders = Invoke-SqliteQuery -DataSource $DataSource -Query "
+                SELECT mo.TypeName, mo.Price, mo.VolumeRemain
+                FROM market_order mo
+                WHERE mo.IsBuyOrder = 0
+                    AND mo.TypeName = @TypeName
+                ORDER BY mo.Price ASC;" -SqlParameters @{"TypeName"=$typeName}
+            $output = New-Object -Type PSObject
+            $output | Add-Member -Type NoteProperty -Name "TypeName" -Value $typeName
+            $output | Add-Member -Type NoteProperty -Name "BuyOrders" -Value $buyOrders
+            $output | Add-Member -Type NoteProperty -Name "SellOrders" -Value $SellOrders
+            $output
+        }
+    }
+    finally {
+        Set-StrictMode -Version Latest
+    }
 }
 
 #function Get-MarketOrder {
