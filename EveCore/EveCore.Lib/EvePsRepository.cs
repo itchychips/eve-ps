19724ba77af4c177af63a7dcb43f03abd5c55f6a
Donny Johnson <itchychips@itchyeyes.net>
2022-07-17 00:49:25 -0500

    Add AGPL license headers to EveCore source files.

--- EveCore/EveCore.Lib/EvePsRepository.cs
+++ EveCore/EveCore.Lib/EvePsRepository.cs
@@ -1,4 +1,17 @@
-﻿using EveCore.Lib.Types;
+﻿// This file is part of Eve-PS.
+//
+// Eve-PS is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Eve-PS is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Affero Public License for more details.
+//
+// You should have received a copy of the GNU Affero Public License along with
+// Eve-PS. If not, see <https://www.gnu.org/licenses/>.
+using EveCore.Lib.Types;
 using System.Data;
 
 namespace EveCore.Lib
